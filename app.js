dc6927179f30ebacf231043fa4b49a24e927f1f6
Junaid Raza <88501739+MuhammadJunaidRaza786@users.noreply.github.com>
2024-03-20 01:11:25 +0500

    Guess Gaming using while loop

---
+++ app.js
@@ -0,0 +1,43 @@
+let targetNum = Math.floor(Math.random() * 10) + 1;
+
+let userInp = parseInt(prompt("Guess the number"));
+
+// if (userInp === targetNum) {
+//   alert("You won the Game");
+// }
+
+
+
+while (userInp !== targetNum) {
+  targetNum = Math.floor(Math.random() * 10) + 1;
+  
+  if (!userInp) {
+    alert("Invalid Input")
+}
+  userInp = parseInt(prompt("Wrong Guess !! , Again Guess the Number"));
+
+  if (userInp === targetNum) {
+    alert("You won the Game");
+    break;
+  }
+  console.log(userInp);
+  console.log(targetNum);
+}
+
+
+
+
+
+
+
+// if (!userInp) {
+//   alert("invalid input");
+// }
+// while (userInp != targetNum) {
+//   userInp = parseInt(prompt("Wrong Guess the number again"));
+//   if (userInp === targetNum) {
+//     alert("congrats you win a Rooh afza");
+//     break;
+//   }
+//   console.log(userInp, targetNum);
+// }
